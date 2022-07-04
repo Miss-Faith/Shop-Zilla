d435d6b528383338a4b479a88a2878aa643b528f
Williamsoditi <williamsoditi99@gmail.com>
2022-07-04 10:02:35 +0300

    Feat : Adds urls and configures templates

--- shopzilla/urls.py
+++ shopzilla/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+    path('logout/', views.logout_then_login, name='logout'),]
