c9c46a7a17cc8c9afc7fed90da205a679e53cb4f
martin023 <mainamartin023@gmail.com>
2022-07-05 16:37:16 +0300

    Update display results page

--- app/views.py
+++ app/views.py
@@ -19,21 +19,27 @@ def get_results(request):
 
     name = []
     price= []
+    image= []
+    location= []
     all_items = []
     for item in jiji_items:
         jiji_item_name = item.find('h4',class_="qa-advert-title").text
         jiji_item_price = item.find('p',class_="b-list-advert__item-price").text
-        
-        
+        jiji_item_image = item.find('img').get('src')
+        jiji_item_location = item.find('div', class_="b-list-advert__item-info").text
 
         name.append(jiji_item_name)
         price.append(jiji_item_price)
+        image.append(jiji_item_image)
+        location.append(jiji_item_location)
 
-    for (a, b) in zip(name, price):
+    for (a, b ,c ,d) in zip(name, price,image,location):
         
         names = {
             'name': a,
-            'price': b
+            'price': b,
+            'image': c,
+            'location': d
         } 
         
         all_items.append(names)
