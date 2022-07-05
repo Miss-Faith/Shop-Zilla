5f02c1cd8c2ca66f24a2eab6ed10588287393322
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-07-05 18:09:48 +0300

    feat: this commit updates views.py

--- app/views.py
+++ app/views.py
@@ -8,29 +8,29 @@ import requests
 # Create your views here.
 def get_results(request):
     
-    url_jiji = "https://jiji.co.ke/search?query=johnie+walker"
+    url_jumia = ""
 
-    jiji_results = requests.get(url_jiji)
+    jumia_results = requests.get(url_jumia)
 
-    jiji_soup = BeautifulSoup(jiji_results.text, 'html.parser')
+    jumia_soup = BeautifulSoup(jumia_results.text, 'html.parser')
 
-    jiji_items = jiji_soup.find_all('div', class_="qa-advert-list-item")
+    jumia_items = jumia_soup.find_all('div', class_="qa-advert-list-item")
 
     name = []
     price= []
     image= []
     location= []
     all_items = []
-    for item in jiji_items:
-        jiji_item_name = item.find('h4',class_="qa-advert-title").text
-        jiji_item_price = item.find('p',class_="b-list-advert__item-price").text
-        jiji_item_image = item.find('img').get('src')
-        jiji_item_location = item.find('div', class_="b-list-advert__item-info").text
+    for item in jumia_items:
+        jumia_item_name = item.find('h4',class_="qa-advert-title").text
+        jumia_item_price = item.find('p',class_="b-list-advert__item-price").text
+        jumia_item_image = item.find('img').get('src')
+        jumia_item_location = item.find('div', class_="b-list-advert__item-info").text
 
-        name.append(jiji_item_name)
-        price.append(jiji_item_price)
-        image.append(jiji_item_image)
-        location.append(jiji_item_location)
+        name.append(jumia_item_name)
+        price.append(jumia_item_price)
+        image.append(jumia_item_image)
+        location.append(jumia_item_location)
 
     for (a, b ,c ,d) in zip(name, price,image,location):
         
