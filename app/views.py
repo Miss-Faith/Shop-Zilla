35f839c09debcaac72dc5b01966f5f24f25d97b5
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-07-05 12:20:39 +0300

    Update search function

--- app/views.py
+++ app/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
-# def home(request):
-#     return HttpResponse('Welcome Home') 
-
 def home(request):
     return render(request, 'home.html')
 
