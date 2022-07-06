10d86d646aab37e1505e4be3f066d3f7e2614810
martin023 <mainamartin023@gmail.com>
2022-07-06 12:56:34 +0300

    Add copia data fetcher

--- app/views.py
+++ app/views.py
@@ -50,6 +50,46 @@ def get_jumia_data():
 
     return all_jumia_products
 
+def get_copia_data():
+
+    url_copia = 'https://copia.co.ke/?s=chair&post_type=product&title=1&excerpt=0&content=0&categories=0&attributes=0&tags=0&sku=1&orderby=date-DESC&ixwps=1'
+    copia_results = requests.get(url_copia)
+    copia_soup = BeautifulSoup(copia_results.text, 'html.parser')
+    copia_items = copia_soup.find_all('div', class_='product-small')
+
+    copia_product_name = []
+    copia_product_price = []
+    copia_product_image = []
+    
+    all_copia_products = []
+
+    for item in copia_items:
+        
+        copia_item_name = item.find('p', class_="woocommerce-loop-product__title").text
+        copia_item_price = item.find('bdi').text
+        copia_item_image = item.find('img').get('data-src')
+        
+
+        copia_product_name.append(copia_item_name)
+        copia_product_price.append(copia_item_price)
+        copia_product_image.append(copia_item_image)
+            
+
+        for (a, b ,c ) in zip(copia_product_name, copia_product_price,copia_product_image):
+            
+            copia_product = {
+                'name': a,
+                'price': b,
+                'image': c,
+                
+            } 
+            
+            all_copia_products.append(copia_product)
+            # print(all_copia_products)
+
+    return all_copia_products
+
+
 def get_results(request):
     
     url_jiji = "https://jiji.co.ke/search?query=johnie"
@@ -87,13 +127,14 @@ def get_results(request):
         
         all_items.append(names)
         
-
+    all_copia_products = get_copia_data()
     all_jumia_products = get_jumia_data()
-    print(len(all_jumia_products))
+    print(len(all_copia_products))
     context={
         'title': 'Results',
         'all_jumia_items': all_jumia_products,
         'names': all_items,
+        'all_copia_items': all_copia_products
     }
 
     return render(request, 'results.html',context)
