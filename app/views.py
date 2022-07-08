7e6f9b4bc59b0eb0960a44413afabc262a675643
Williamsoditi <williamsoditi99@gmail.com>
2022-07-08 23:48:26 +0300

    Feat : Refractors code to build cards

--- app/views.py
+++ app/views.py
@@ -40,7 +40,7 @@ def search(request):
         jiji_item_location = item.find('div', class_="b-list-advert__item-info").text.split(',')[0]
 
         name.append(jiji_item_name)
-        # price.append(jiji_item_price)
+        price.append(jiji_item_price)
         image.append(jiji_item_image)
         location.append(jiji_item_location)
 
