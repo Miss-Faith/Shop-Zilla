a409a56698d90b33f6dbdb9d184ceb5f226c291f
Williamsoditi <williamsoditi99@gmail.com>
2022-07-09 23:47:31 +0300

    Feat : Adds scroll to top button  on templates

--- app/views.py
+++ app/views.py
@@ -35,12 +35,12 @@ def search(request):
     total_items = []
     for item in jiji_items:
         jiji_item_name = item.find('h4',class_="qa-advert-title").text
-        jiji_item_price = int(item.find('p',class_="b-list-advert__item-price").text.replace('KSh ', '').replace('\n ', '').replace(' ', '').replace(',', ''))
+        # jiji_item_price = int(item.find('p',class_="b-list-advert__item-price").text.replace('KSh ', '').replace('\n ', '').replace(' ', '').replace(',', ''))
         jiji_item_image = item.find('img').get('src')
         jiji_item_location = item.find('div', class_="b-list-advert__item-info").text.split(',')[0]
 
         name.append(jiji_item_name)
-        price.append(jiji_item_price)
+        # price.append(jiji_item_price)
         image.append(jiji_item_image)
         location.append(jiji_item_location)
 
