ae1c031710401fbc63f799919d2df6cffa0fe419
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-07-14 00:18:14 +0300

    update tracker model

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ import datetime as dt
 # Create your models here.
 class Tracker(models.Model):
   url = models.URLField()
-  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tracker')
-  time = models.DateTimeField(auto_now_add=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tracker', null= True)
+  time = models.DateTimeField(auto_now_add=True, null=True)
 
   def __str__(self):
     return str(self.user.username)
