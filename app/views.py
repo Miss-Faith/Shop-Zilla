d65761c4855b53d3ec49ae54b1f6aa8b6f89c6df
Williamsoditi <williamsoditi99@gmail.com>
2022-07-13 15:57:27 +0300

    fix : Code refractoring

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 from bs4 import BeautifulSoup
 import requests
@@ -169,4 +169,3 @@ def search(request):
     }
 
     return render(request, 'search.html', context)
-
