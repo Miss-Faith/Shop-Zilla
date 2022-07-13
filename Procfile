d6195557a8a51f13c596ef5655597f937ec7a6a9
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-07-14 01:24:46 +0300

    update deployment settings

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn shopzilla.wsgi --log-file -
+web: gunicorn shopzilla.wsgi
--- shopzilla/settings.py
+++ shopzilla/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+from pathlib import Path
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
