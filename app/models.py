9dafeebb961c3a99c81a5c5b7d60c630ab609b9f
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-07-13 23:47:09 +0300

    add tracker model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Tracker(models.Model):
+    url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tracker')
+    time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+    return str(self.user.username)
+
+  @classmethod
+  def save_post(self):
+    self.save()
